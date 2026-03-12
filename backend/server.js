8b7707d0a6b64f5836b43ba729dd0dfe065f36d2
omkarbhanji <142101115+omkarbhanji@users.noreply.github.com>
2026-03-12 20:11:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,8 @@
+require('dotenv').config();
 const app = require("./app");
 const mongoose = require("mongoose");
 
-
-
-
-
-
-
-const DB = 
-    "mongodb+srv://bhanjiomkar:Sucasa2021@cluster0.2q2oe.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0"
+const DB = process.env.MONGODB_URL
 
     mongoose.connect(DB).then((con) => {
         console.log("DB connection successful !");
